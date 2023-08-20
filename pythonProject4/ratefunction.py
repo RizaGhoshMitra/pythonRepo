36a93a5131e1c930c8be617521c8ca02bb43bd67
rizaghoshmitra <20111569@tafe.wa.edu.au>
2023-08-20 11:09:58 +0800

    first change

--- pythonProject4/ratefunction.py
+++ pythonProject4/ratefunction.py
@@ -2,6 +2,7 @@ hour=input("enter your hour:")
 rate=input("enter yor rate:")
 pay=float(hour)*float(rate)
 #print("pay",pay)
+#changing
 def payable(rate1,pay1):
     return "payable",float(rate1) * float(pay1)
 finalPay=payable(rate,hour)
