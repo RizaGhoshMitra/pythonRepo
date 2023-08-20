3f85850952f400618d4435ae7edbcb241aff6257
rizaghoshmitra <20111569@tafe.wa.edu.au>
2023-08-20 11:04:03 +0800

    my first commit

---
+++ pythonProject4/function exercise2.py
@@ -0,0 +1,30 @@
+x=int(input("enter the name of names"))
+i=0
+my_input=[]
+while i<x :
+    y=str(input("enter the name {} ".format(i)))
+    my_input.append(y)
+    i+=1
+    print(my_input)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pythonProject4/function programme exercise 1.py
@@ -0,0 +1,6 @@
+def sum(numbers):
+    total=0
+    for x in numbers:
+        total+=x
+    return total
+print(sum((1000,1789,2345)))
